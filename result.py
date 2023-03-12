cfcfb12efa5269848bd815f41300c8031a223dc5
inbal226 <inbalbala226@walla.com>
2023-03-12 13:11:16 +0200

    usd

--- result.py
+++ result.py
@@ -1,7 +1,5 @@
-
-
+lst = [1, 2, 3]
 file1 = open("test_result.txt", "w")
-lst= [1,2,3]
 
 file1.writelines(str(lst))
 file1.close()
@@ -10,7 +8,4 @@ file1 = open("test_result.txt", 'r')
 print(file1.read())
 file1.close()
 
-file1 = open("test_result.txt", "w")
-L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
-file1.writelines(L)
-file1.close()
+
